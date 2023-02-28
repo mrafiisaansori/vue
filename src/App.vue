3d9647769aa64fbcaf99a24f384cb49bb50627ff
mrafiisaansori <isarafi13@gmail.com>
2023-02-28 15:34:03 +0700

    gas

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>IDShop</h1>
-    <p class="animated fadeInRight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quasi dolores sunt dolorem omnis amet eaque odio! Velit eaque culpa, adipisci molestiae sed nihil, totam quisquam ad ut aspernatur architecto.</p>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-    <price value="4.23"></price>
+    <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum"></price-slider>
+    <product-list :products="products" :maximum="maximum" @add="addItem"></product-list>
   </div>
 </template>
 
 <script>
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import Price from './components/Price.vue';
+// import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import ProductList from './components/ProductList.vue';
+import PriceSlider from './components/PriceSlider.vue';
 export default {
   name: "app",
+  data: function(){
+    return {
+      maximum:50,
+      products: [],
+      cart: [],
+      sliderStatus: true
+    }
+  },
   components: {
-    FontAwesomeIcon, 
-    Price
+    // FontAwesomeIcon,
+    ProductList,
+    PriceSlider
+  },
+  mounted: function() {
+    fetch('https://hplussport.com/api/products/order/price')
+    .then(response => response.json())
+    .then(data => {
+        this.products = data;
+    });
+  },
+  methods:{
+    addItem: function (product) {
+        let productIndex;
+        let productExist = this.cart.filter(function (item,index){
+          if(item.product.id==Number(product.id)){
+            productIndex = index;
+            return true;
+          }
+          else{
+            return false;
+          }
+        });
+
+        if(productExist.length){
+          this.cart[productIndex].qty++
+        } else {
+          this.cart.push({product:product, qty:1});
+        }
+    },
   }
 };
 </script>
